8d499dc1bc42b01ad8bf240210715fbcfa1782f2
Ian King <ian.antking@gmail.com>
2019-05-28 19:08:20 +0100

    got basic app running

---
+++ index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const app = require('./src/app');
+
+const port = process.env.PORT || 4000;
+
+mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
+  console.log(`App running on 127.0.0.1:${port}`);
+  app.listen(port);
+});
