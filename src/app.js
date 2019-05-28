a32f2a581ecfec9503bd8085b9795d26d88abf72
Ian King <ian.antking@gmail.com>
2019-05-28 19:30:50 +0100

    set up static serving

--- src/app.js
+++ src/app.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('*', (_, res) => {
+  res.redirect('/');
+});
+
 module.exports = app;
