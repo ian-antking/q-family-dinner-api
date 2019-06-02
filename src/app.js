18aebfc4633521dc8c058a90a5ff634d06efb1a2
Ian King <ian.antking@gmail.com>
2019-06-02 18:36:25 +0100

    connected up event router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const eventRouter = require('./routes/event');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+
+app.use('/event', eventRouter);
 
 app.get('*', (_, res) => {
   res.redirect('/');
---
+++ src/controllers/event.js
@@ -0,0 +1,3 @@
+exports.getEvents = (_, res) => {
+  res.status(200).send();
+};
