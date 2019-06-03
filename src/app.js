ba4e0a9777af0d65992c123cd42658b97df48476
Ian King <ian.antking@gmail.com>
2019-06-03 19:01:14 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const eventRouter = require('./routes/event');
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
 app.use('/event', eventRouter);
 app.use('/user', userRouter);
