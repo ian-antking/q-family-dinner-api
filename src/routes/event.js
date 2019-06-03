09047aef8c2a25a9ed7313ec7f412f2a35245c01
Ian King <ian.antking@gmail.com>
2019-06-03 18:12:24 +0100

    added auth middleware to sensitive routes

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const eventController = require('../controllers/event');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', eventController.postEvent);
+router.post('/', auth, eventController.postEvent);
 router.get('/', eventController.getEvents);
-router.put('/:id', eventController.updateEvent);
-router.delete('/:id', eventController.deleteEvent);
+router.put('/:id', auth, eventController.updateEvent);
+router.delete('/:id', auth, eventController.deleteEvent);
 
 module.exports = router;
