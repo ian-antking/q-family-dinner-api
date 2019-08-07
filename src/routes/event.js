9694b6204e9a9983185e3ecf465b44a61ebf01df
Ian King <ian.antking@gmail.com>
2019-08-07 19:06:37 +0100

    changed get events

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const eventController = require('../controllers/event');
-const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', auth, eventController.postEvent);
 router.get('/', eventController.getEvents);
-router.put('/:id', auth, eventController.updateEvent);
-router.delete('/:id', auth, eventController.deleteEvent);
 
 module.exports = router;
